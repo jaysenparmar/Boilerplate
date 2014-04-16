41f614d590c040d5feef8412c4f032f4e91caf6c
Jaysen Parmar <jparmar@ucsd.edu>
2014-04-15 17:30:34 -0700

    Blank Page when authorizing Facebook credentials

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 var app = express();
-var dotenv = require('dotenv');
-dotenv.load();
+var graph = require('fbgraph');
 
 //route files to load
 var index = require('./routes/index');
+var facebook = require('./routes/auth/facebook');
+var loggedin = require('./routes/loggedin');
 
-//database setup - uncomment to set up your database
-//var mongoose = require('mongoose');
-//mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/DATABASE1);
+//Place in .env file and change conf. to process.env
+/*
+var conf = {
+    client_id:      '231469240376504'
+  , client_secret:  'da9ba9f03fcb8d3bf262e9e9a2a08cb1'
+  , scope:          'email, user_about_me, user_birthday, user_location, publish_stream'
+  , redirect_uri:   'localhost:3000/auth/facebook'
+};
+*/
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
@@ -21,8 +28,45 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
 
-//routes
+
+//view files
 app.get('/', index.view);
+app.get('/auth/facebook', function(req, res) {
+
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+   //Facebook authentication using Oauth
+	var authUrl = graph.getOauthUrl({
+	  "client_id": process.env.client_id
+	, "redirect_uri": process.env.redirect_uri //change this to heroku website later in .env
+	, "scope": 'email, user_location'
+	});
+
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+    return;
+  }
+
+  // code is set
+  // we'll send that and get the access token
+ graph.authorize({
+	  "client_id": process.env.client_id
+	, "redirect_uri": process.env.redirect_uri
+	, "client_secret": process.env.client_secret
+	, "code": req.query.code
+ }, function(err, facebookRes) {
+	res.redirect('/loggedin');
+ });
+
+});
+
+app.get('/loggedin', loggedin.view);
+
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
--- auth.js
+++ auth.js
@@ -1,52 +1,44 @@
 //load environment variables
 var dotenv = require('dotenv');
 dotenv.load();
-
-/**
-* Add your authentication apis here with example like the bottom
-*/
-
-//Add Facebook API Setup
 var graph = require('fbgraph');
-graph.setAccessToken(access_token);
-//Set Facebook AppID and AppSecret
-graph.set('fb_app_id', process.env.facebook_app_id);
-graph.set('fb_app_secret', process.env.facebook_app_secret);
-//Export graph as a parameter to be used by other methods that require it
-exports.graph = graph;
 
-   // get authorization url
-    var authUrl = graph.getOauthUrl({
-        "client_id":     conf.client_id
-      , "redirect_uri":  conf.redirect_uri
-    });
+//Authenticate Facebook User
+exports.login = function(req, res) {
 
-    // shows dialog
-    res.redirect(authUrl);
+  var graph = require('fbgraph');
 
-    // after user click, auth `code` will be set
-    // we'll send that and get the access token
-    graph.authorize({
-        "client_id":      conf.client_id
-      , "redirect_uri":   conf.redirect_uri
-      , "client_secret":  conf.client_secret
-      , "code":           req.query.code
-    }, function (err, facebookRes) {
-      res.redirect('/loggedIn');
-    });
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+   //Facebook authentication using Oauth
+	var authUrl = graph.getOauthUrl({
+	  "client_id": process.env.client_id
+	, "redirect_uri": process.env.redirect_uri //change this to heroku website later in .env
+	, "scope": 'email, user_location, publish_stream'
+	});
 
-graph.get("zuck", function(err, res) {
-  console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+    return;
+  }
+
+  // code is set
+  // we'll send that and get the access token
+ graph.authorize({
+	  "client_id": process.env.client_id
+	, "redirect_uri": process.env.redirect_uri
+	, "client_secret": process.env.client_secret
+	, "code": req.query.code
+ }, function(err, facebookRes) {
+	res.redirect('/loggedin');
+ });
+
 });
+
 //Add Twitter API Setup Here
 //TODO
-
-/** EXAMPLE **
-//add instagram api setup
-var ig = require('instagram-node-lib');
-ig.set('client_id', process.env.instagram_client_id);
-ig.set('client_secret', process.env.instagram_client_secret);
-
-//export ig as a parameter to be used by other methods that require it.
-exports.ig = ig;
-**/
