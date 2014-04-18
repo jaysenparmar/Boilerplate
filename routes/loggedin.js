b66240eb4f8dff1649db329ef95593d19c30db99
Jaysen Parmar <jaysenparmar@gmail.com>
2014-04-18 13:54:53 -0700

    FB Working Finally, displaying data

--- routes/loggedin.js
+++ routes/loggedin.js
@@ -6,14 +6,14 @@ exports.view = function(req, res) {
 
 exports.userinfo = function(req, res){
     
-    graph.get("/me", function(err, res) {
-       //console.log(res);\
-       var data  = [];
-       data = res;
+    graph.get("/me", function(err, response) {
+       console.log(response);
+       //var data  = [];
+       //data = res;
       //pass data off to view function    
-       //res.render('loggedin', data);
+       res.render('loggedin',response);
     });
     //Return data to the webpage
-    res.render('loggedin');
+    //res.render('loggedin');
     
 }
