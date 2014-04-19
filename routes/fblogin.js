9916be2e456f7d494f416d5df8975a7f9dbb010d
Cesar Cisneros <cecisner@ucsd.edu>
2014-04-19 00:07:02 -0700

    heroku

--- routes/fblogin.js
+++ routes/fblogin.js
@@ -6,7 +6,7 @@ var conf = {
     client_id:      '231469240376504'
   , client_secret:  'da9ba9f03fcb8d3bf262e9e9a2a08cb1'
   , scope:          'user_about_me, user_birthday, user_location, user_likes, user_status, friends_relationships, friends_likes'
-  , redirect_uri:   'http://infinite-springs-3439.herokuapp.com/auth/facebook'
+  , redirect_uri:   'http://safe-beyond-4638.herokuapp.com/auth/facebook'
   //, redirect_uri:   'http://localhost:3000/auth/facebook'
 };
 
